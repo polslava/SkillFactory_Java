6757bf5c7919944c585d13db9887bd23d4e6998d
polslava <polslava16@yandex.ru>
2023-10-13 21:18:47 +0800

    20231013

--- JavaCore_m6_a5/src/Student.java
+++ JavaCore_m6_a5/src/Student.java
@@ -3,6 +3,11 @@ public class Student extends Person{
     int age;
     int grade;
 
+    /*public Student(String name, int age, int grade) {
+        this.name=name;
+        this.age=age;
+        this.grade=grade;
+    }*/
 public Student (String name, int age){
         super(name, age);
 }
