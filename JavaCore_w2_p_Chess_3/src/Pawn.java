09f21e06f3fdfb673281c41627d481067f2471f1
polslava <polslava16@yandex.ru>
2023-10-20 22:41:50 +0800

    20231020

--- JavaCore_w2_p_Chess_3/src/Pawn.java
+++ JavaCore_w2_p_Chess_3/src/Pawn.java
@@ -14,24 +14,25 @@ public class Pawn extends ChessPiece{
         if (toLine!=line ) {
             if (toLine > 0 && toLine < 8 && toColumn==column) {
 
-                if (((toLine == line-2)&&(line==6)&& (this.getColor()=="White"))
+                /*if (((toLine == line-2)&&(line==6)&& (this.getColor()=="White"))
                         ||((toLine == line+2)&& (line==1)&& (this.getColor()=="Black"))){
                     return true;} else
                         if (((toLine==line-1)&& (this.getColor()=="White"))
                         ||((toLine == line+1)&& (this.getColor()=="Black"))){
                         return true;
-                        } else {return false;}
+                        } else {return false;}*/
                          //проверял порядок ходов, где белые внизу массива, но тестер даёт ошибку
 
 
 
-                /*if (((toLine == line+2)&&(line==1)&& (this.getColor()=="White"))
+                if (((toLine == line+2)&&(line==1)&& (this.getColor()=="White"))
                         ||((toLine == line-2)&& (line==6)&& (this.getColor()=="Black"))){
                     return true;} else
                 if (((toLine==line+1)&& (this.getColor()=="White"))
                         ||((toLine == line-1)&& (this.getColor()=="Black"))){
                     return true;
-                } else {return false;} */ //проверял порядок ходов, где белые сверху массива, но тестер даёт ошибку
+                } else {return false;}
+                //проверял порядок ходов, где белые сверху массива, работает в тестере
 
                     } else {return false;}
                 } else {return false;}
