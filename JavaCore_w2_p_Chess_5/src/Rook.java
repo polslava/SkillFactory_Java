dee25f6d27b7bb3ceff867d05dfb73725f977838
polslava <polslava16@yandex.ru>
2023-10-21 22:10:58 +0800

    20231021

---
+++ JavaCore_w2_p_Chess_5/src/Rook.java
@@ -0,0 +1,31 @@
+public class Rook extends ChessPiece{
+    public Rook(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toLine!=line && toColumn!=column) {
+            if (toLine > 0 && toLine < 8 && toColumn > 0 && toColumn < 8) {
+                if (((toLine > line) && (toColumn == column ) )
+                    || ((toLine < line) && (toColumn == column ))
+                    || ((toLine == line) && (toColumn > column ))
+                    || ((toLine == line) && (toColumn < column )))
+                {
+                    return true;}
+                else {return false;}}
+            else {return false;}}
+        else {return false;}
+    }
+
+
+    @Override
+    public String getSymbol() {
+        return "R";
+    }
+}
