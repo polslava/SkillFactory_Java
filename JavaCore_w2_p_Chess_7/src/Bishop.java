769d66db10d4e623066025d3dff7b74689bcf5f3
polslava <polslava16@yandex.ru>
2023-10-23 23:19:35 +0800

    20231023

---
+++ JavaCore_w2_p_Chess_7/src/Bishop.java
@@ -0,0 +1,34 @@
+import static java.lang.Math.*;
+public class Bishop extends ChessPiece{
+    public Bishop(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toLine!=line && toColumn!=column) {
+            if (toLine >= 0 && toLine < 8 && toColumn >= 0 && toColumn < 8) {
+                if (((toLine -line) == (toColumn -column ))||
+                ((toLine -line) == -1*(toColumn -column ))) {
+                    return true;}
+                else {
+                    return false;}
+
+            }else {
+                return false;}
+        }
+                    else {
+                    return false;}
+
+    }
+
+    @Override
+    public String getSymbol() {
+        return "B";
+    }
+}
---
+++ JavaCore_w2_p_Chess_7/src/Pawn.java
@@ -0,0 +1,46 @@
+public class Pawn extends ChessPiece{
+    public Pawn(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toLine!=line ) {
+            if (toLine >= 0 && toLine < 8 && toColumn==column) {
+
+                /*if (((toLine == line-2)&&(line==6)&& (this.getColor()=="White"))
+                        ||((toLine == line+2)&& (line==1)&& (this.getColor()=="Black"))){
+                    return true;} else
+                        if (((toLine==line-1)&& (this.getColor()=="White"))
+                        ||((toLine == line+1)&& (this.getColor()=="Black"))){
+                        return true;
+                        } else {return false;}*/
+                         //проверял порядок ходов, где белые внизу массива, но тестер даёт ошибку
+
+
+
+                if (((toLine == line+2)&&(line==1)&& (this.getColor()=="White"))
+                        ||((toLine == line-2)&& (line==6)&& (this.getColor()=="Black"))){
+                    return true;} else
+                if (((toLine==line+1)&& (this.getColor()=="White"))
+                        ||((toLine == line-1)&& (this.getColor()=="Black"))){
+                    return true;
+                } else {return false;}
+                //проверял порядок ходов, где белые сверху массива, работает в тестере
+
+                    } else {return false;}
+                } else {return false;}
+
+    }
+
+    @Override
+    public String getSymbol() {
+        return "P";
+    }
+}
