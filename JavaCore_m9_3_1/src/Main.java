57c89837910b40db71f63aca6f202b05710a5541
polslava <polslava16@yandex.ru>
2023-10-27 22:55:57 +0800

    20231027

--- JavaCore_m9_3_1/src/Main.java
+++ JavaCore_m9_3_1/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         String home = System.getProperty("user.home");
         File file = new File(home + File.separator + "Test" + File.separator + "Testing" + File.separator + "Java.txt");
         //не понятна работа с файлами в Linux: файл не создаётся всё равно
-        long length = file.length(); //(1024*1024); //MB
+        long length = file.length()/(1024*1024); //MB
         System.out.println(length);//6
     }
 }
