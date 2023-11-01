b0fa3ef16869f53e027d54136ddf544aff556806
polslava <polslava16@yandex.ru>
2023-11-01 19:46:39 +0800

    20231101

--- javacore_w1_t3/src/SeaBoard.java
+++ javacore_w1_t3/src/SeaBoard.java
@@ -19,8 +19,11 @@ public class SeaBoard {
         for (int i = 0; i < 10; i = i + 1) {
             for (int j = 0; j < 10; j = j + 1) {
                 field_1[i][j] = check(i,j);
+                System.out.print(field_1[i][j]);
             }
+            System.out.println();
         }
+
         return field;
     }
    /*public void SetField(int line, int column, String issue){
@@ -31,10 +34,24 @@ public class SeaBoard {
     }
     public String check(int line, int column){
         String result="";
-        if (field[line][column] == ".")
-        {
-            result= ".";
-        } else if (field[line][column] == "m") {
+
+            if ((field[line][column] == ".") &&
+                    ((line>0&&(field[line-1][column] == "h" || field[line-1][column] == "d")) || //поиск прямых клеток подбитостей
+                            (line<9&&(field[line+1][column] == "h" || field[line+1][column] == "d"))||
+                            (column>0&&(field[line][column-1] == "h" || field[line][column-1] == "d"))||
+                            (column<9&&(field[line][column+1] == "h" || field[line][column+1] == "d"))
+                    ||(line>0&&column>0&&(field[line-1][column-1] == "h" || field[line-1][column-1] == "d")) || //поиск угловых клеток подбитостей
+                        (line>0&&column<9&&(field[line-1][column+1] == "h" || field[line-1][column+1] == "d")) ||
+                        (line<9&&column>0&&(field[line+1][column-1] == "h" || field[line+1][column-1] == "d")) ||
+                            (line<9&&column<9&&(field[line+1][column+1] == "h" || field[line+1][column+1] == "d"))
+                    )){
+                result= "#";
+            } else
+            if (field[line][column] == ".")
+            {
+                result= ".";
+            } else
+            if (field[line][column] == "m" ) {
             result= "#";
         }else if (field[line][column] == "h" || field[line][column] == "d") {
             result= "x";
