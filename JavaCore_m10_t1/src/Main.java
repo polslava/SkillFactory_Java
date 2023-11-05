8dd88635ad4cb63aab2796aa7d432f6f8cc345c1
polslava <polslava16@yandex.ru>
2023-11-05 22:40:42 +0800

    20231105

--- JavaCore_m10_t1/src/Main.java
+++ JavaCore_m10_t1/src/Main.java
@@ -3,6 +3,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Solution sol = new Solution();
-        sol.getReceipt();
+        System.out.println(sol.getReceipt());
     }
 }
--- JavaCore_m10_t1/src/Solution.java
+++ JavaCore_m10_t1/src/Solution.java
@@ -1,8 +1,9 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
 
 public class Solution {
-    public void getReceipt() throws IOException {
+    public String getReceipt() throws IOException {
         // write your code here
        // FileInputStream fileInputStream = new FileInputStream("src//receipt.txt");
         int c; //номер символа
@@ -25,9 +26,11 @@ public class Solution {
             i = fis.read();
         }
         fis.close();
-        System.out.println(result);
-        int[] semicoms=new int[10];
-        int s=0;
+        //System.out.println(result);
+        int[] semicoms1=new int[10];
+        int s1=0;
+        int[] semicoms2=new int[10];
+        int s2=0;
         int[] retEnts=new int[10];
         int r=0;
         int[] dots=new int[10];
@@ -35,32 +38,53 @@ public class Solution {
         int N=result.length();
         for (int i2=0; i2<N;i2=i2+1)
         {
-            System.out.print(result.substring(i2,i2+1));
+            //System.out.print(result.substring(i2,i2+1));
             if (result.substring(i2,i2+1).equals(";")){
                 semicom=semicom+1;
                 if((semicom%2)==0){
-                semicoms[s]=i2;
-                s=s+1;}
+                    semicoms2[s2]=i2;
+                    s2=s2+1;}
+                else
+                {
+                    semicoms1[s1]=i2;
+                    s1=s1+1;}
             }
-            if (result.substring(i2,i2+1).equals("\n")){
-                retEnt=retEnt+1;
-                retEnts[r]=i2;
-                r=r+1;
-            }
+
             if (result.substring(i2,i2+1).equals(".")){
                 dot=dot+1;
                 dots[d]=i2;
                 d=d+1;
             }
+            if (i2!=N-1)
+            {if (result.substring(i2,i2+1).equals("\n")){
+                retEnt=retEnt+1;
+                retEnts[r]=i2;
+                r=r+1;
+            }}else{retEnt=retEnt+1;
+                retEnts[r]=i2+1;}
         }
-        System.out.println();
+        /*System.out.println();
         System.out.println(semicom);
         System.out.println(retEnt);
         System.out.println(dot);
+
+         */
+        double res_receipt = 0;
         for (int j=0;j<d;j=j+1){
-            System.out.println(result.substring(semicoms[j],dots[j]-semicoms[j]));
-            System.out.println(semicoms[j]);
-            System.out.println(dots[j]);
+
+            //System.out.println(semicoms[j]);
+            //System.out.println(dots[j]);
+            //System.out.println(dots[j]-semicoms[j]);
+            //System.out.println(result.substring(semicoms1[j]+1,semicoms2[j]));
+            //System.out.println(result.substring(semicoms2[j]+1,retEnts[j]));
+            res_receipt = res_receipt+
+                    Double.parseDouble(result.substring(semicoms1[j]+1,semicoms2[j]))*
+                    Double.parseDouble(result.substring(semicoms2[j]+1,retEnts[j]));
         }
+        //DecimalFormat decimalFormat = new DecimalFormat("#,##0.00"); //1,051.40 вместо 1051.40
+        //String numberAsString = decimalFormat.format(res_receipt);
+        //System.out.println(numberAsString); //250,00
+        return String.valueOf(res_receipt)+"0"; //250.0
+        //return numberAsString;
     }
 }
