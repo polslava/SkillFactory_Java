97a80da2a8fb3a07679f1412bac21d1e94e6a760
polslava <polslava16@yandex.ru>
2023-11-06 22:56:03 +0800

    20231106

--- javacore_w1_t3/src/Main.java
+++ javacore_w1_t3/src/Main.java
@@ -103,17 +103,20 @@ game.makeMove(2, 1);
          game.makeMove(3, 1);
           Arrays.deepToString(game.getField());
            */
+//===============================
 
-        SeaBoard board = new SeaBoard();
+      /*  SeaBoard board = new SeaBoard();
         board.shoot(0, 0, "m");
         board.shoot(2, 0, "h");
         board.shoot(6, 9, "h");
         board.shoot(2, 1, "d");
         board.getField();
-/*
+*/
+
+        SeaBoard seaBoard = new SeaBoard();
         seaBoard.shoot(2, 0, "h");
                 seaBoard.shoot(2, 8, "d");
-        Arrays.deepToString(seaBoard.getField());
+       // Arrays.deepToString(seaBoard.getField());
           seaBoard.shoot(6, 9, "h");
         seaBoard.shoot(2, 2, "m");
         seaBoard.shoot(0, 0, "m");
@@ -125,24 +128,31 @@ game.makeMove(2, 1);
         seaBoard.shoot(9, 7, "h");
         seaBoard.shoot(8, 7, "d");
          seaBoard.shoot(2, 1, "d");
-           Arrays.deepToString(seaBoard.getField());
-         seaBoard.check(i, j);
-              seaBoard.shoot(2, 0, "h");
-            seaBoard.shoot(2, 8, "d");
-             Arrays.deepToString(seaBoard.getField());
-              seaBoard.shoot(6, 9, "h");
-           seaBoard.shoot(2, 2, "m");
-        seaBoard.shoot(0, 0, "m");
-        seaBoard.shoot(5, 2, "m");
-                seaBoard.shoot(7, 0, "h");
-               seaBoard.shoot(7, 1, "h");
-           seaBoard.shoot(7, 2, "h");
-          seaBoard.shoot(7, 3, "d");
-        seaBoard.shoot(9, 7, "h");
-         seaBoard.shoot(8, 7, "d");
-        seaBoard.shoot(2, 1, "d");
-         Arrays.deepToString(seaBoard.getField());
-          seaBoard.check(i, j);
+         seaBoard.getField();
+
+
+
+//=============================
+        //Squirrel s = new Squirrel(new int[] {2, 5, -3, -1});
+        //System.out.println(s.maxNuts());
+        ///Squirrel s2 = new Squirrel(new int[] {1, -2, 3, 1, -1, -4, 2, 3});
+        //System.out.println(s2.maxNuts());
+
+        /*Squirrel s = new Squirrel(new int[] {2, 5, -3, -1});
+        String res = s.getNuts(3) + "";
+        res += s.maxNuts();*/
+        /*Squirrel s2 = new Squirrel(new int[] {1, -2, -4, 7, 3, -2, -2, 1, -1});
+        System.out.println( s2.maxNuts() );
+        Squirrel s3 = new Squirrel(new int[] {-2, 2, 4, -6, -2, -1, -2, 1});
+        System.out.println( s3.maxNuts());
+        System.out.println( (new Squirrel(new int[] {1})).maxNuts());
+        System.out.println( (new Squirrel(new int[] {-1})).maxNuts());
+        System.out.println( (new Squirrel(new int[] {1, -2})).maxNuts());
+        System.out.println( (new Squirrel(new int[] {1, -2, 4, 6, 3, -2, -2, -3, -1})).maxNuts());
+        System.out.println( (new Squirrel(new int[] {1, 1, 1, 1, 1})).maxNuts());
+        System.out.println( (new Squirrel(new int[] {1, 1, 1, 1, -1})).maxNuts());
+        System.out.println( (new Squirrel(new int[] {1, -1, 2, -2, -7, -7, 2, 1})).maxNuts());
 */
+
     }
 }
--- javacore_w1_t3/src/SeaBoard.java
+++ javacore_w1_t3/src/SeaBoard.java
@@ -1,6 +1,6 @@
 public class SeaBoard {
 //не сдано
-
+//нужен механизм поиска полностью подбитого корабля
 
     private String[][] field = new String[10][10];
     public SeaBoard()
@@ -30,13 +30,64 @@ public class SeaBoard {
         field[line][column]=issue;
    }*/
     public void shoot(int line, int column, String issue){
-        field[line][column]=issue;
+
+        field[line][column]=issue; //фиксация хода в ячейку
+       if(issue=="d")
+       {
+           int i=line;
+
+           outerloop1:
+               for (int j=column;j<9;j=j+1)
+               {
+                   if (field[i][j]!="." &&field[i][j]!="m") {
+                       this.field[i][j]=issue;
+                   } else
+                   if (field[i][j]=="." ||field[i][j]=="m"){
+                       break outerloop1;}
+               }
+
+           outerloop2:
+
+               for (int j=column;j>=0;j=j-1)
+               {
+                   if (field[i][j]!="." &&field[i][j]!="m") {
+                       this.field[i][j]=issue;
+                   }else
+                   if (field[i][j]=="." ||field[i][j]=="m")
+                   { break outerloop2;
+                    }
+               }
+
+           int j=column;
+           outerloop3:
+           for ( i=line;i<9;i=i+1)
+           {
+               if (field[i][j]!="." &&field[i][j]!="m") {
+                   this.field[i][j]=issue;
+               } else
+               if (field[i][j]=="." ||field[i][j]=="m"){
+                   break outerloop3;}
+           }
+
+           outerloop4:
+
+           for ( i=line;i>=0;i=i-1)
+           {
+               if (field[i][j]!="." &&field[i][j]!="m") {
+                   this.field[i][j]=issue;
+               }else
+               if (field[i][j]=="." ||field[i][j]=="m")
+               { break outerloop4;
+               }
+           }
+
+       }
     }
     public String check(int line, int column){
         String result="";
-
+    //поиск пустых клеток и клеток раненного и подбитого корабля
             if ((field[line][column] == ".") &&
-                    ((line>0&&(field[line-1][column] == "h" || field[line-1][column] == "d")) || //поиск прямых клеток подбитостей
+                    /*((line>0&&(field[line-1][column] == "h" || field[line-1][column] == "d")) || //поиск прямых клеток подбитостей
                             (line<9&&(field[line+1][column] == "h" || field[line+1][column] == "d"))||
                             (column>0&&(field[line][column-1] == "h" || field[line][column-1] == "d"))||
                             (column<9&&(field[line][column+1] == "h" || field[line][column+1] == "d"))
@@ -44,17 +95,28 @@ public class SeaBoard {
                         (line>0&&column<9&&(field[line-1][column+1] == "h" || field[line-1][column+1] == "d")) ||
                         (line<9&&column>0&&(field[line+1][column-1] == "h" || field[line+1][column-1] == "d")) ||
                             (line<9&&column<9&&(field[line+1][column+1] == "h" || field[line+1][column+1] == "d"))
-                    )){
-                result= "#";
+                    )*/
+                    ((line>0&&(field[line-1][column] == "d")) || //поиск прямых клеток подбитостей
+                            (line<9&&(field[line+1][column] == "d"))||
+                            (column>0&&( field[line][column-1] == "d"))||
+                            (column<9&&( field[line][column+1] == "d"))
+                            ||(line>0&&column>0&&( field[line-1][column-1] == "d")) || //поиск угловых клеток подбитостей
+                            (line>0&&column<9&&( field[line-1][column+1] == "d")) ||
+                            (line<9&&column>0&&(field[line+1][column-1] == "d")) ||
+                            (line<9&&column<9&&( field[line+1][column+1] == "d"))
+                    )
+            ){
+                result= "#"; //клетки рядом с попаданиями и подбитым кораблём отображены решёткой, а должно быть только рядом
+                // с подбитым кораблём (нет механизма поиска полностью подбитого корабля)
             } else
             if (field[line][column] == ".")
             {
-                result= ".";
+                result= "."; //пустые клетки отображены точкой
             } else
             if (field[line][column] == "m" ) {
-            result= "#";
+            result= "#"; //промахи отображены решёткой
         }else if (field[line][column] == "h" || field[line][column] == "d") {
-            result= "x";
+            result= "x"; //попадания и подбитый отображены х
         }
         return result;
     }
