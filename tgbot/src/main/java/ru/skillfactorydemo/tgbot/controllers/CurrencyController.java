e8cdc95e3eac315bc0e4373c3bc22fd6bb73f360
polslava <polslava16@yandex.ru>
2024-08-02 22:56:44 +0800

    20240802

---
+++ tgbot/src/main/java/ru/skillfactorydemo/tgbot/controllers/CurrencyController.java
@@ -0,0 +1,19 @@
+package ru.skillfactorydemo.tgbot.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skillfactorydemo.tgbot.service.CentralRussianBankService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CurrencyController {
+
+    private final CentralRussianBankService centralRussianBankService;
+
+    @GetMapping("/getCurrencies")
+    public List<ValuteCursOnDate> getValuteCursOnDate() throws Exception {
+        return centralRussianBankService.getCurrenciesFromCbr();
+    }
+}
---
+++ tgbot/src/main/java/ru/skillfactorydemo/tgbot/dto/GetCursOnDateXml.java
@@ -0,0 +1,17 @@
+package ru.skillfactorydemo.tgbot.dto;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+@XmlRootElement(name = "GetCursOnDateXML", namespace = "http://web.cbr.ru/")
+@XmlAccessorType(XmlAccessType.FIELD) //Указываем, как получить/записать значения в поля данного объекта
+@Data //Геттеры и сеттеры
+public class GetCursOnDateXml {
+
+    @XmlElement(name = "On_date", required = true, namespace = "http://web.cbr.ru/") //Указание на то, в каком теге XML должно быть данное поле
+    protected XMLGregorianCalendar onDate;
+}
